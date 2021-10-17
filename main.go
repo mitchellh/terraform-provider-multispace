67d9f0a94be9f3822c6511c190dbdacea3fa633d
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-10-17 11:46:10 -0700

    prepare main for 0.1

--- main.go
+++ main.go
@@ -38,8 +38,7 @@ func main() {
 	opts := &plugin.ServeOpts{ProviderFunc: provider.New(version)}
 
 	if debugMode {
-		// TODO: update this string with the full name of your provider as used in your configs
-		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/scaffolding", opts)
+		err := plugin.Debug(context.Background(), "registry.terraform.io/mitchellh/multispace", opts)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
